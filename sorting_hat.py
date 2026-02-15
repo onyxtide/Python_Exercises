169a6f441226b4ce14304a993b2eff8a7aecd661
Mario A. <90323650+onyxtide@users.noreply.github.com>
2026-02-16 00:21:58 +0200

    Update sorting_hat.py
    Completed the full code

--- sorting_hat.py
+++ sorting_hat.py
@@ -16,5 +16,36 @@ if Q1 == 1:
 elif Q1 == 2: 
      Hufflepuff += 1;  Slytherin += 1
 else: 
-     print ("Wrong input, please use numbers only!") 
-  
+     print ("Wrong input") 
+
+Q2 = int(input("When I'm dead, I want people to remember me as: 1)The Good, 2)The Great, 3)The Wise, 4) The Bold? "))
+
+if Q2 == 1:
+  Hufflepuff += 2
+elif Q2 == 2:
+  Slytherin += 2
+elif Q2 == 3:
+  Ravenclaw += 2
+elif Q2 == 4:
+  Gryffindor += 2
+else:
+  print("Wrong input")
+
+Q3 = int(input("Which kind of instrument most pleases your ear? 1) The violin, 2) The trumpet, 3)The piano, 4)The drum "))
+
+if Q3 == 1:
+  Slytherin += 4
+elif Q3 == 2:
+  Hufflepuff += 4
+elif Q3 == 3:
+  Ravenclaw += 4
+elif Q3 == 4:
+  Gryffindor += 4
+else:
+  print("Wrong input")
+
+print("You are this amount of Slytherin:", Slytherin)
+print("You are this amount of Hufflepuff:", Hufflepuff)
+print("You are this amount of Ravenclaw:", Ravenclaw)
+print("You are this amount of Gryffindor:", Gryffindor)
+
