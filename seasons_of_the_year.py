ff218103d75182c00802bbfa5772e1045a2c80af
Mario A. <90323650+onyxtide@users.noreply.github.com>
2026-02-16 23:38:47 +0200

    Create seasons_of_the_year.py

---
+++ seasons_of_the_year.py
@@ -0,0 +1,13 @@
+#Here we ask the user to state the month as an integer, and we print the season it belongs to 
+month = int(input("What is the number of the current month? "))
+
+if month == 1 or month == 2 or month == 3:
+  print('Winter 🌨️')
+elif month == 4 or month == 5 or month == 6:
+  print('Spring 🌱')
+elif month == 7 or month == 8 or month == 9:
+  print('Summer 🌻')
+elif month == 10 or month == 11 or month == 12:
+  print('Autumn 🍂')
+else:
+  print('Invalid')
