ce2b617e7cbcc7a8bffc197ee4ddffe6ebcf2916
Mario A. <90323650+onyxtide@users.noreply.github.com>
2026-02-16 23:26:45 +0200

    Create highschool_usa.py

---
+++ highschool_usa.py
@@ -0,0 +1,13 @@
+#  This code is based on the highschool american system
+grade = int(input("Hey there! What grade are you in? "))
+
+if grade == 9:
+  print("Freshman")
+elif grade == 10:
+  print("Sophomore")
+elif grade == 11:
+  print("Junior")
+elif grade == 12:
+  print("Senior")
+else:
+  print("TBD")
