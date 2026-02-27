25f0393e280e2999c3916f565666ad115884ce0b
Mario A. <90323650+onyxtide@users.noreply.github.com>
2026-02-27 20:05:18 +0200

    Create todo_list.py

---
+++ 05_lists/todo_list.py
@@ -0,0 +1,15 @@
+# In our first lesson on lists, we learned about the list's proper syntax, list indexes, and the list slicing method. 
+# Instructions: Print the first item and print the second item using a negative index. Next, use slicing to print the third, fourth, and fifth items.
+
+todo = ['🏦 Get quarters.',
+'🧺 Do laundry.',
+'🌳 Take a walk.',
+'💈 Get a haircut.',
+'🍵 Make some tea.',
+'💻 Complete Lists chapter.',
+'💖 Call mom.',
+'📺 Watch My Hero Academia.']
+
+print(todo[0])
+print(todo[-7])
+print(todo[2:5])
