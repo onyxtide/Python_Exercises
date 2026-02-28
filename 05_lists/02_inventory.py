dc7204a863c0b9fad8b595b36988fdf028304451
Mario A. <90323650+onyxtide@users.noreply.github.com>
2026-02-28 20:30:10 +0200

    Create 02_inventory.py

---
+++ 05_lists/02_inventory.py
@@ -0,0 +1,11 @@
+#Learning basic built-in functions for lists in Python
+
+#Exercise: Suppose the holidays are around the corner, and the North Pole elves need your help to have thousands of LEGO toys packed up and ready for shipping.
+#Can you figure out the following information using built-in list functions?
+#Which LEGO part are the elves running low on?
+#Is there a LEGO part that the elves are overstocking? 
+
+lego_parts = [8980, 7323, 5343, 82700, 92232, 1203, 7319, 8903, 2328, 1279, 679, 589]
+
+print(min(lego_parts))
+print(max(lego_parts))
