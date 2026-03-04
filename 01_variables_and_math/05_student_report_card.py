39088ff91c256f4666af907e87356f3b2cde4927
Mario A. <90323650+onyxtide@users.noreply.github.com>
2026-03-04 19:20:33 +0200

    Create 05_student_report_card.py

---
+++ 01_variables_and_math/05_student_report_card.py
@@ -0,0 +1,13 @@
+#In this simple code, we learn how to create variables and print their different types
+
+name = 'Alice'
+print(name, type(name))
+
+is_student = True
+print(is_student, type(is_student))
+
+age = 20
+print(age, type(age))
+
+score = 80.5
+print(score, type(score))
