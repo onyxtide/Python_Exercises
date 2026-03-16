76bacce123deed3caf2ed3cf258c6b8dcf0589d3
Mario A. <90323650+onyxtide@users.noreply.github.com>
2026-03-16 23:38:40 +0200

    Create academy_awards.py

---
+++ 05_lists/academy_awards.py
@@ -0,0 +1,21 @@
+#The best pictures list misses the movies that won the Oscar in the years after 2019. Try to add them using list methods. 
+best_pictures = [
+  '2019 - Parasite',
+  '2018 - Green Book',
+  '2017 - The Shape of Water',
+  '2016 - Moonlight',
+  '2015 - Spotlight',
+  '2014 - Birdman',
+  '2013 - 12 Years a Slave',
+  '2012 - Argo',
+  '2011 - The Artist'
+]
+
+best_pictures.insert(0, "2020 - Nomadland")
+best_pictures.insert(0, "2021 - CODA")
+best_pictures.insert(0, "2022 - Everything Everywhere All at Once")
+best_pictures.insert(0, "2023 - Oppenheimer")
+best_pictures.insert(0, "2024 - Anora")
+best_pictures.insert(0, "2025 - One Battle after Another")
+
+print(best_pictures)
