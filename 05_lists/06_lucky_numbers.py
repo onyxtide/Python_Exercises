773bc7d7c876b05280ff32f6d6fd7934c3551c8b
Mario A. <90323650+onyxtide@users.noreply.github.com>
2026-03-17 23:56:08 +0200

    Create 06_lucky_numbers.py

---
+++ 05_lists/06_lucky_numbers.py
@@ -0,0 +1,18 @@
+import random
+
+my_numbers = []
+winning_numbers = []
+
+
+for _ in range(5):
+  num = random.randint(1, 69)
+  my_numbers.append(num)
+  win_num = random.randint(1, 69)
+  winning_numbers.append(win_num)
+
+
+my_numbers.append(random.randint(1,26))
+winning_numbers.append(random.randint(1,26))
+
+print(my_numbers)
+print(winning_numbers)
